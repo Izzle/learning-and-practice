b2139a16cd702dfc1e1305b617d6a9543d22b67a
Ron Martin <ronmartin89@gmail.com>
2016-05-14 21:13:58 -0700

    Add files via upload

--- rainbow.py
+++ rainbow.py
@@ -9,8 +9,24 @@
 
 # pseudocode because I haven't figured out why I can't store a hash value in a dictionary (I know that would be a hash of a hash)
 # Im very novice at python and this is meant to be more of an outline
+import hashlib
+# declaring dictionaries for the rainbow
+# need to create a constructor to slim the code down
+rainbow1 = {}
+rainbow2 = {}
+rainbow3 = {}
+rainbow4 = {}
+rainbow5 = {}
+rainbow6 = {}
 
 allPasswordz from wordlist.txt # Need to store a word list into a variable
 passwordz in allPasswordz      # Need to slice up each password individually
-    rainbow{passwordz} = hash(passwordz) # Iterate each password into a dictionary and generate its hash as the value
+    rainbow1[passwordz] = hashlib.md5(passwordz) # Iterate each password into a dictionary and generate its hash as the value
+    rainbow2[passwordz] = hashlib.sha1(passwordz)
+    rainbow3[passwordz] = hashlib.sha224(passwordz)
+    rainbow4[passwordz] = hashlib.sha256(passwordz) # WAY too repetitive haha
+    rainbow5[passwordz] = hashlib.sha384(passwordz)
+    rainbow6[passwordz] = hashlib.sha512(passwordz)
 
+hashz from hashlist.txt  # Used to separate out a list of hashed passwords
+# Need to compare hashes from hashlist.txt to all known hashes in our rainbow
