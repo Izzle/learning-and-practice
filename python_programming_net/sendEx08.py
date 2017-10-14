1569bec8794a0f6c6f3679ea952d358d757fac04
Ron Martin <ronmartin89@gmail.com>
2017-10-14 03:42:10 -0700

    Writing to files and global variables workarounds

---
+++ python_programming_net/sendEx08.py
@@ -0,0 +1,45 @@
+# Exercise 13 
+# Global and Local variables in Python 3
+
+# Local variable a
+a = 6
+
+def example():
+	b = 5
+	print(b)
+	print(a) # Works, we can ACCESS 'a'
+	print(a+5) # Also works
+	# a += 6 ERROR, 'a'  is not a global variable
+
+example()
+
+# Global variable z
+z = 3
+
+def example_two():
+	global z # makes 'z' a global variable
+	print(z) 
+	z += 5   # This now works!
+	print(z)
+
+example_two()
+
+# IMPORTANT!
+# If you CAN'T set Global variables (big projects, sometimes bad practice)
+# This is how to get around not being able to SET Global variables,
+# but still harness the power of them.
+
+x = 2
+
+def example_three():
+	globx = x    # Sets the value of x to globx, since x can still be ACCESSED
+	print(globx) # 2
+	globx += 4   # Works because globx is a local variable
+	print(globx) # 6 
+
+	return globx # returns the value of globx
+
+x = example_three() # x is now equal to globx!
+
+print(x) # 6
+
---
+++ python_programming_net/sendEx09_modules.py
@@ -0,0 +1,15 @@
+# Exercise 16 & 17 
+# Writing and Appending to Files
+# Yes I know my naming convention is not standard.. I wrote it at 2AM 
+
+text = 'Sample Text to Save\nNew line!'
+
+# Using the open() function to Write the file. WARNING - Write will overwrite any old file.
+# First parameter is the name of the file to open. If none exists, it'll create it.
+# Second parameter 'w' tells it to Write to the specifed file.
+saveFile = open('exampleFile.txt', 'w') # Notifys Python our intentions
+
+saveFile.write(text) # Tells Python to write text
+saveFile.close() # IMPORTANT to close or the file will hang open for a long time
+
+# Appendding to a file
