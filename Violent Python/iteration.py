5e2c93df96a0994c7ea52939b409f2cae5c346b6
Ron Martin <ronmartin89@gmail.com>
2017-10-16 20:07:01 -0700

    Added projects from Violent Python

---
+++ Violent Python/iteration.py
@@ -0,0 +1,10 @@
+# Violent Python Iteration example
+
+for x in range(1,255): # range() iterates from the first number UP TO(but not including) the last
+	print('192.168.95.'+str(x)) # IP addresses 192.168.95.1 - *.*.*.254
+
+portList = [21,22,25,80,110]
+for port in portList:
+	print(port)
+
+	
---
+++ Violent Python/iteration2.py
@@ -0,0 +1,40 @@
+# Violent Python Chapter 1
+# This is a continuation of the exercise in iteration.py
+
+import socket
+
+def retBanner(ip, port):
+	try:
+		socket.setdefaulttimeout(2)
+		s = socket.socket()
+		s.connect((ip, port))
+		banner = s.recv(1024)
+		return banner
+	except:
+		return
+
+def checkVulns(banner):
+	f = open('vuln_banner.txt', 'r')
+	for line in f.readlines():
+		if line.strip('\n') in banner:
+			print("[+] Server is vulnerable: " + banner.strip('\n'))
+		else:
+			print('[-] No vulnerable Servers found.')
+
+def main():
+	"""portList contains all possible vunerable ports we are 
+ 	  interested in checking: telnet, SSH, smtp, http, imap,
+ 	  and https services.
+	"""
+	portList = [21,22,25,80,110] 
+
+	for x in range(1,255):
+		ip = '192.168.95.' + str(x)
+		for port in portList:
+			banner = retBanner(ip, port)
+			if banner:
+				print('[+] ' + ip + ': ' + banner)
+				checkVulns(banner)
+
+if __name__ == '__main__': 
+	main()
