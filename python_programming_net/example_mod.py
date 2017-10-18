eb79b112bb91a3f34e28b37b264dd68d1fb1ea4b
Ron Martin <ronmartin89@gmail.com>
2017-10-17 18:55:33 -0700

    Testing creating local modules and running them.

---
+++ python_programming_net/example_mod.py
@@ -0,0 +1,6 @@
+# An example module to practice referencing from.
+# Python looks for modules in lib, site-packages, and locally.
+
+
+def ex(data):
+    print(data)
---
+++ python_programming_net/sendEx13.py
@@ -0,0 +1,10 @@
+# Exercise 24
+# Custom modules
+#
+
+
+import example_mod
+
+example_mod.ex('I hope this works.')
+print('...\n' * 3)
+print('It worked!')
