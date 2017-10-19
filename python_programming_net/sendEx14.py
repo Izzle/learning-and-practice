fdd4055645a3f5a4ccbecc94df4860fb89cc0575
Ron Martin <ronmartin89@gmail.com>
2017-10-19 03:39:21 -0700

    Added multidimensional lists to experiment with

--- python_programming_net/sendEx14.py
+++ python_programming_net/sendEx14.py
@@ -10,3 +10,24 @@ print(example_str)
 x = '!'
 example_str = example_str + x
 print(example_str)
+
+# Multi-dimensional lists
+a = [[3, 2], [22, 12], [56, 57], [8, 9], [7, 13]]
+print(a)
+# Prints the third element ie the first nested list
+# i.e. [56, 57]
+print(a[2])
+# Targets the third element, then targets the second element of that list
+# i.e. [57]
+print(a[2][1])
+
+# Three dimensional list
+b = [
+    [[2, 2], [4, 6]],
+    [[7, 2], [9, 42]],
+    [[3, 13], [7, 7]]
+    ]
+print(b)
+print('First element of the first list:', b[0])
+print('Second element of previous list:', b[0][1])
+print('First element of previous list:', b[0][1][0])
