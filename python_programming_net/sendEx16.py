32802aacc091befc87288d74b86afcf63dbf3eb9
Ron Martin <ronmartin89@gmail.com>
2017-10-21 03:22:32 -0700

    Learned about using sys.argv to pass argument into Python from the terminal or another language! Also updated syntax list for syntax.py

--- python_programming_net/sendEx16.py
+++ python_programming_net/sendEx16.py
@@ -39,4 +39,42 @@ sys.stderr.write('This is stderr text\n')
 sys.stderr.flush()
 sys.stdout.write('This is stdout text\n')
 
+# sys.argv takes any arguments passed to it and puts them in a list.
+# Important! We can pass arguments into a Python file using the commmandline
+# - which means we can pass arguments from almost any other programming
+# languages PHP to Python, Ruby to Python, Perl " ",  batch files etc.
+#
+# If you run this script in the terminal with 'python3.6 sendEx16.py'
+# it would be: sys.argv == 'sendEx16.py' because we passed that argument to it
+# Or we can run 'python3.6 sendEx16.py "LOOK AT THAT!"' and sys.argv would get
+# passed both values. So then, sys.argv == ['sendEx16.py', 'LOOK AT THAT!']
+# The default value of sys.argv is the current filepath
+
+# This will print out the value in a list i.e. ['sendEx16.py']
+print(sys.argv)
+
+if len(sys.argv) > 1:
+    # This will print out the value specifically, without list brackets or ''
+    print(sys.argv[1])
+    # We can manipulate the values that come in Python!
+    print(float(sys.argv[1]) + 5)
+
+
+# Example how to pass an argument from another language (PHP, JS) to Python
+def main(arg):
+    print(arg)
+    if type(arg) == str or float:
+        globx = int(arg)
+    elif type(arg) == int:
+        globx = arg
+    else:
+        globx = '[-] Error!'
+
+    return globx
+
+
+# Notice how we can pass sys.argv[1] asking for the index immediately
+# This is possible because the list is created at terminal when run
+main(sys.argv[1])
+
 
--- python_programming_net/syntax.py
+++ python_programming_net/syntax.py
@@ -35,6 +35,11 @@ print, pretty cool, huh?
 
 
 ''')
+
+# =======================================
+#          Arithmetic Operators
+# =======================================
+
 1 + 1
 # Subtraction
 5 - 2
@@ -42,8 +47,106 @@ print, pretty cool, huh?
 4 * 4
 # Division. Gives you 2.5, a Float
 5 / 2
+
+# FLoor DivisionThe division of operands where the result is the
+# quotient in which the digits after the decimal point are removed.
+# But if one of the operands is negative, the result is floored,
+# i.e., rounded away from zero (towards negative infinity) −
+13 // 4
+# Modulus
+7 % 2
 # Exponent, 'Four to the power of Four'. Gives you 256
 4 ** 4
+
+# =======================================
+#   Comparison & Assignment Operators
+# =======================================
+a = 4
+b = 2
+
+# Equal
+a == b
+# Not equal
+a != b
+# a Greater Than b ?
+a > b
+# a Less than b ?
+a < b
+# Greater than or equal to
+a >= b
+# Lesser than or equal to
+a <= b
+
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a **= b
+a //= b
+
+
+# =======================================
+#    Membership & Identity Operators
+# =======================================
+
+# Python’s membership operators test for membership in a sequence,
+# such as strings, lists, or tuples.
+
+memList = [1, 2, 3, 4, 5]
+
+# x in y, here in results in a 1 if x is a member of sequence y.
+truth = 2 in memList
+print("the truth is", truth)
+
+# x not in y, here not in results in a 1 if x is not a member of sequence y.
+diet_truth = 'weirdo' in memList
+print("diet truth is:", diet_truth)
+
+
+# Identity operators compare the MEMORY LOCATIONS of two objects.
+
+# Evaluates to true if the variables on either side of the operator point to
+# the same OBJECT and false otherwise. Example:
+# x is y, here is results in 1 if id(x) equals id(y).
+first_dict = {'Fish': 13, 'Cat': 4, 'Dog': 7, 'Mice': 2302}
+different_dict = first_dict
+
+if different_dict is first_dict:
+    print('\"different_dict is a copycat!\"\n  - first_dict')
+else:
+    print('\"pfft.. first_dict is so unoriginal\"\n  - different_dict')
+
+
+# Evalutes to false if the point to the same object, true otherwise. Ex:
+# x is not y, here is not results in 1 if id(x) is not equal to id(y).
+j = 777
+k = 777
+print('Both \'j\' and \'k\' equal', j, 'but they were each set to equal', k,
+      'separately and we never set j=k or k=j.')
+print('The question then becomes: Since they are both the same type:', type(k),
+      '\nAnd have the same value. Does Python store them in the same memory'
+      ' location?', j is k)
+print('But a guy I know said they are saved in different spots:', j is not k)
+
+
+# =======================================
+#      Boolean & Bitwise Operators
+# =======================================
+
+# Boolean logic uses English:
+# and       or        not
+a and b
+j or k
+not x
+
+
+
+
+
+# =======================================
+#            Tuples & Lists
+# =======================================
 
 # Tuple notation. Tuples are immutable (they cannot be changed).
 # Tuples are generated faster and are iterated faster than Lists.
