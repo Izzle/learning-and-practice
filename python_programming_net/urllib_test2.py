308f32e0d10b840331eb3c8295ff80de5ae6ef29
Ron Martin <ronmartin89@gmail.com>
2017-10-24 01:46:52 -0700

    Updated User-Agent in the HTTP header so that Google won't block us

--- python_programming_net/urllib_test2.py
+++ python_programming_net/urllib_test2.py
@@ -10,6 +10,7 @@ except Exception as e:
     print(str(e))
 
 try:
+    # I hardcoded the URL for simplicity of the exercise
     url = 'https://www.google.com/search?q=test'
     
     # HEADERS are all the data you send in when you visit a website:
@@ -17,5 +18,20 @@ try:
     # Python says the browser is 'python/urllib/python3.6' NOT SO SNEEKY
     headers = {}
     headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-    req = urllib.request.Request(url)
+    # Our request to the URL, modifying our User-Agent
+    req = urllib.request.Request(url, headers=headers)
+    # urllib.request.openurl() can take either a STRING containing a URL or
+    # a .Request OBJECT as an argument and RETURNS the data. If you send a
+    # .Request OBJECT you can specify HTTP header info like User-Agent.
+    resp = urllib.request.openurl(req)
+    respData = resp.read()
+
+    # Printing respData to IDLE would lag out the console. Instead, we'll
+    # save to a file.
+    saveFile = open('withHeaders.txt', 'w')
+    saveFile.write(str(respData))
+    saveFile.close()
+
 except Exception as e:
+    print(str(e))
+
