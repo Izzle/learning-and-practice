5fff6ee2333eb26bf076d68304fc927d04e292e8
Ron Martin <ronmartin89@gmail.com>
2017-10-24 03:30:44 -0700

    Basic first parse using REGEX to search for any text between paragraph tags(<p>).

--- python_programming_net/parsing_websites.py
+++ python_programming_net/parsing_websites.py
@@ -15,4 +15,17 @@ req = urllib.request.Request(url, data)
 resp = urllib.request.urlopen(req)
 respData = resp.read()
 
-print(respData)
+# Used print to test code. Commenting out unless needed to test.
+#print(respData)
+
+# Searching for ANYTHING between <p> tags
+# (.*?) is the pattern to search
+# . "means any character except new line"
+# * applies to the '.' and means "0 or more repetitions"
+# ? applies to  '.*' and means "Match 0 or 1 repitions of that"
+#
+# Also, remember to convert respData to a string or re.findall won't work!
+paragraphs = re.findall(r'<p>(.*?)</p>', str(respData))
+
+for eachP in paragraphs:
+    print(eachP)
