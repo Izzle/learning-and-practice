5a7f73050d6684891edc98465b8076f8bc33c95d
Ron Martin <ronmartin89@gmail.com>
2017-10-24 22:55:47 -0700

    Set Window dimensions, title, and added a 'Quit' button. 'Quit' button is currently non-functional.

--- python_programming_net/tkinter_fun.py
+++ python_programming_net/tkinter_fun.py
@@ -1,4 +1,4 @@
-# Exercise 38
+# Exercise 38, 39
 # Tkinter
 
 from tkinter import *
@@ -10,13 +10,27 @@ class Window(Frame):
     def __init__(self, master=None):
         # __init__ is called when ever an object of the class is constructed
         Frame.__init__(self, master)
-
         # Our Master widget or main frame
         self.master = master
+        # Not from Tk library. We're creating this function below.
+        self.init_window()
+
+    # initialize our window
+    def init_window(self):
+        # The title of our window is "GUI"
+        self.master.title("GUI")
+        # Says: We are filling up the windows. Adjust dimensions as needed.
+        self.pack(fill=BOTH, expand=1)
+        # Button() is function within tkinter
+        quitButton = Button(self, text='Quit')
+
+        quitButton.place(x=0, y=0)
 
 
 # root Window
 root = Tk()
+# Dimensions of our window
+root.geometry("600x450")
 
 
 app = Window(root)
