f9cc73f1b3c73d4296b5b9433d23eaa07c302085
Ron Martin <ronmartin89@gmail.com>
2017-10-24 22:08:01 -0700

    Initial commit: called the Tkinter constructor for our main Window/Frame.

---
+++ python_programming_net/tkinter_fun.py
@@ -0,0 +1,23 @@
+# Exercise 38
+# Tkinter
+
+from tkinter import *
+
+
+class Window(Frame):
+
+    # Parent Widget, initialized per the tk documentation
+    def __init__(self, master=None):
+        # __init__ is called when ever an object of the class is constructed
+        Frame.__init__(self, master)
+
+    # Our Master widget or main frame
+    self.master = master
+
+
+root = Tk()
+
+app = Window(root)
+
+
+
