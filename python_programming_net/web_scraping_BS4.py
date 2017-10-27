f11a81c4498c733998242737b9be2aa4a6638feb
Ron Martin <ronmartin89@gmail.com>
2017-10-27 03:41:29 -0700

    Got the sauce (successful urlopen). Now to parse through the data.

--- python_programming_net/web_scraping_BS4.py
+++ python_programming_net/web_scraping_BS4.py
@@ -1,1 +1,16 @@
 # Web scraping and parsing with Beautfiul Soup 4 
+
+import bs4 as bs
+import urllib.request
+
+# Sauce and Soup are standard variable names in the BS4 community (like foo bar)
+sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+# Test print.
+print(soup)  # Works! Prints HTML that looks like the source code
+print(soup.title)         # <title> Greatest Page in USA</title>
+print(soup.title.name)    # title
+print(soup.title.string)  # Greatest Page in USA
+
