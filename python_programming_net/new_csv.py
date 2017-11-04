5832d4e9fc02c126d6f102aea077a93a53c0c711
Ron Martin <ronmartin89@gmail.com>
2017-11-03 23:42:04 -0700

    Everything is working- Reading from CSV, sorting columns of interest out, searching for important information in each element using REGEX, and writing a new CSV. HOWEVER, there is something I fundamentally dont understand about iterating over csv_reader - a DictReader instance - which is preventing me from creating the RIGHT CSV. WIll research and fix soon..

--- python_programming_net/new_csv.py
+++ python_programming_net/new_csv.py
@@ -14,50 +14,70 @@ def fix(re_search, csv_col):
     return diction
 
 
+def merge_two_dicts(x, y):
+    """Given two dicts, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
+
+
 with open('../Work/org_address_book.csv', 'r') as csvfile:
     csv_reader = csv.DictReader(csvfile, delimiter=',')
+
     # The lists that will hold our CSV columns
     first_names = []
     last_names = []
-    nick_names = []
     emails = []
     block_notes = []
-    # print(csv_reader.fieldnames)
+    # ISSUE IS WITH CSV READER BEING REFERENCED TWICE!
+    # 111111111111111111111111
+    for row in csv_reader:
+        # Rows are where each category of relevant data is saved
+        FN = row['First Name']
+        EM = row['E-mail Address']
+        note = row['Notes']
+        # Creates lists of each column
+        first_names.append(FN)
+        emails.append(EM)
+        block_notes.append(note)
 
-    # for line in csv_reader:
-    #     #print(line['Notes'])
-    #     print(line)
+    # Finding data in 'Notes' using our fix function - this will parse out
+    # data and sort it using REGEX
+    dataDict = {}
+    dataDict['Email'] = fix(r"Email\sAddress\:\s[A-Za-z0-9.@_]+", block_notes)
+    dataDict['First Name'] = fix(r"First\sName\:\s[A-Za-z0-9.@_' ]+", block_notes)
+
+    # Slicing out the header data from the contents
+    for line in dataDict['Email']:
+        # Cutting out the substring that says 'Email Address: '
+        if dataDict['Email'][line][0:15] == 'Email Address: ':
+            line_len = len(dataDict['Email'][line])
+            dataDict['Email'][line] = dataDict['Email'][line][15:line_len]
+        else:
+            pass
+
+    # YES I KNOW REPEATING IS BAD
+    # But I have a time crunch and this is free work so cut me some slack!
+    # It works, doesnt it?
+    for line in dataDict['First Name']:
+        # Cutting out the substring that says 'Email Address: '
+        if dataDict['First Name'][line][0:12] == 'First Name: ':
+            line_len = len(dataDict['First Name'][line])
+            dataDict['First Name'][line] = dataDict['First Name'][line][12:line_len]
+        # If NO NAME then we use EMAIL
+        if dataDict['First Name'][line] == 'NONE':
+            dataDict['First Name'][line] = dataDict['Email'][line]
+        else:
+            pass
 
     with open('../Work/newer_address_book.csv', 'w') as new_file:
         fieldnames = csv_reader.fieldnames
-
         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
         csv_writer.writeheader()
-        
-        # Goes over each list in the CSV
+
         for line in csv_reader:
-            # Goes over each field in the list
-            for field in fieldnames:
-                print(line[field])
+            print('FUCK')
+            csv_writer.writerow(line)
 
-
-    #print(dir(csv_reader))
-    #for line in csv_reader:
-        #print(line[20:33])
-'''
-    for row in csv_reader:
-        # Rows are where each category of relevant data is saved
-        FN = row[0]
-        LN = row[2]
-        NN = row[3]
-        EM = row[27]
-        note = row[30]
-        # Creates lists of each column
-        first_names.append(FN)
-        last_names.append(LN)
-        nick_names.append(NN)
-        emails.append(EM)
-        block_notes.append(note)
-    
-    print(block_notes)
-'''
+        # for line in csv_reader:
+        #     print(csv_reader)
