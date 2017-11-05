e8d20376871870bf6705592fe7c0359c22e6fdbe
Ron Martin <ronmartin89@gmail.com>
2017-11-04 20:46:14 -0700

    Updated and fixed spacing

--- python_programming_net/web_scraping_BS4.py
+++ python_programming_net/web_scraping_BS4.py
@@ -1,10 +1,11 @@
-# Web scraping and parsing with Beautfiul Soup 4 
+# Web scraping and parsing with Beautfiul Soup 4
 
 import bs4 as bs
 import urllib.request
 
 # Sauce and Soup are standard variable names in the BS4 community (like foo bar)
-sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+sauce = urllib.request.urlopen(
+    'https://pythonprogramming.net/parsememcparseface/').read()
 
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
@@ -12,9 +13,11 @@ soup = bs.BeautifulSoup(sauce, 'lxml')
 print(soup)  # Works! Prints HTML that looks like the source code
 print(soup.title)         # <title> Greatest Page in USA</title>
 print(soup.title.name)    # title
-print(soup.title.string)  # returns a "navigable string" so child tags return 'NONE'
+# returns a "navigable string" so child tags return 'NONE'
+print(soup.title.string)
 print(soup.title.text)    # returns unicode, meaning it returns EVERYTHING
-print(soup.get_text())    # Gets text from all sorts of tags (span, code, div, etc)
+# Gets text from all sorts of tags (span, code, div, etc)
+print(soup.get_text())
 
 print(soup.p)             # First 'p' element
 print(soup.nav)           # First 'nav' element, LIKELY THE MAIN NAV OF THE PAGE!
