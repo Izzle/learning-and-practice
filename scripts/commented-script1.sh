5b3cf3a54d18c2d4e7ec4567ce1e82d24711b8b6
= <ronmartin89@gmail.com>
2017-11-12 19:57:22 -0800

    Commented verison of script1.sh used for testing out debugging with vim

--- scripts/commented-script1.sh
+++ scripts/commented-script1.sh
@@ -2,23 +2,23 @@
 # This script clears the terminal, displays a greeting and gives information
 # about currently connected users. The two example variables are set and displayed.
 
-clear
+clear                           # Clear terminal Window
 
 echo "The script starts now."
 
-echo "Hi, $USER!"
+echo "Hi, $USER!"               # dollar sign is used to get content of variable
 echo
 
 echo "I will now fetch you a list of connected users:"
 echo
-w
-echo
+w                               # show who is logged on and
+echo                            # what they are doing
 
 echo "I'm setting two variables now."
-COLOR="purple"
-VALUE="7"
-echo "This is a string: $COLOR"
-echo "This is a number: $VALUE"
+COLOR="purple"                         # set a local shell variable
+VALUE="7"                              # set a local shell variable
+echo "This is a string: $COLOR"        # display content of variable
+echo "This is a number: $VALUE"        # display content of variable
 echo
 
 echo "I'm giving you back your prompt now."
