956c5abc4eb851dd574cd3554813733ce5b1a8d5
= <ronmartin89@gmail.com>
2017-11-12 19:52:09 -0800

    Created a quick reference for learning Vim and started commenting my Shell script.

---
+++ scripts/commented-script1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script clears the terminal, displays a greeting and gives information
+# about currently connected users. The two example variables are set and displayed.
+
+clear
+
+echo "The script starts now."
+
+echo "Hi, $USER!"
+echo
+
+echo "I will now fetch you a list of connected users:"
+echo
+w
+echo
+
+echo "I'm setting two variables now."
+COLOR="purple"
+VALUE="7"
+echo "This is a string: $COLOR"
+echo "This is a number: $VALUE"
+echo
+
+echo "I'm giving you back your prompt now."
+echo
+"
+"
+"
