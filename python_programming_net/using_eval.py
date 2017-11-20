eef1e3567419bfc8aa0fb84a2ba4da1732a49e74
Ron Martin <ronmartin89@gmail.com>
2017-11-19 23:57:33 -0800

    Created a file to experiment with the built-in 'eval' and 'exec' methods.

---
+++ python_programming_net/using_eval.py
@@ -0,0 +1,11 @@
+list_str = '[5, 6, 7, 3, 77, 2]'
+
+list_str = eval(list_str)
+
+print(list_str)
+print(list_str[2])
+
+x = input('code:')
+check_this_out = eval(input('code:'))
+
+print(check_this_out)
