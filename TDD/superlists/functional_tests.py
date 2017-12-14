fd2757e1eab09882b97163754cd4d7a658d0ff65
Ron Martin <ronmartin89@gmail.com>
2017-12-14 02:42:48 -0800

    Deleted the old functional_tests.py and renamed functional_tests2.py after it. Also currently running tests to get lists/views to run.

--- TDD/superlists/functional_tests.py
+++ TDD/superlists/functional_tests.py
@@ -1,15 +1,19 @@
 '''Test Driven Development with Python
 
-   TODO: Add a requirements file
-         Required:
-             -Python 3.6
-             -Django
+   TODO: Finish setup of Dev Environment on each platform
+         that I work on.
+         Windows:
              -Selenium
              -Geckodriver
              -virtualenv
+         macOS:
              -Firefox
-         Recommended:
-             -Git
+             -Geckodriver
+         Linux(VMware):
+             -Complete!
+         Linux(proxmox):
+             -verify virtualenvwrapper PATH
+              (may need to reorder .bashrc entries)      
 
 '''
 
@@ -17,8 +21,29 @@
 # and that its ready for us to work with. 
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('https://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUP(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Sally has heard about a cool new online to-do app.
+        # She wants to check out its homepage
+        self.browser.get('https://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+        # [...to be continued]
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
