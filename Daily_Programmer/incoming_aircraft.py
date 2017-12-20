f7beed5d99fa2f104bbb21461c6a0abae98d80f3
Ron Martin <ronmartin89@gmail.com>
2017-12-20 04:00:32 -0800

    Startig daily programmer challenge.

---
+++ Daily_Programmer/incoming_aircraft.py
@@ -0,0 +1,30 @@
+"""Daily Programmer Challenge 341
+   https://www.reddit.com/r/dailyprogrammer/comments/7es7gj/20171122_challenge_341_intermediate_listening_for/
+
+   Author: Ron Martin
+"""
+
+'''
+
+INPUT:
+2 lists of data, one from each RADAR station (x cord of station,
+                                              y cord of station,
+                                              degrees(0-360) Max sound amp.)
+ex.
+ x | y | degrees
+----------------
+ 0 | 0 | 45
+10 | 0 | 0
+
+OUTPUT:
+Location of aircraft as a grid coordinate (x, y)
+
+Basic triangulation problem
+'''
+
+def y_equals_mx_plus_b_shit():
+	pass
+
+
+def get_a_pen_and_paper():
+	print("Be right back.. basic maths")
